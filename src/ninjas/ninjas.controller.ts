8b9f38313461d85f115f03527817c91dd069ccf9
Juliana kuedjeu <julianakeudjeu@gmail.com>
2023-08-02 12:15:56 +0100

    Completed nest

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,20 +1,45 @@
-import { Controller } from '@nestjs/common';
-import { get } from 'http';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, UseGuards, ValidationPipe } from '@nestjs/common';
+import { query } from 'express';
+import { type } from 'os';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-update.dto';
+import { NinjasService } from './ninjas.service';
+import { BeltGuard } from 'src/belt/belt.guard';
 
 @Controller('ninjas')
+
 export class NinjasController {
+    constructor(private readonly ninjasService: NinjasService) {}
     // GET /ninjas-->[]
-    @get()
-    getNinjas(){
-        return[];
+    @Get()
+    getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks'){
+        //const service = new NinjasService();
+        return this.ninjasService.getNinjas(weapon);
     }
 
     //GET /ninjas/:id-->{}
-    @get(':id')
-    getOneNinja() {
-        return {};
+    @Get(':id')
+    getOneNinja(@Param('id', ParseIntPipe) id: number) {
+        try{
+            return this.ninjasService.getNinja(id);
+        }catch(err){
+            throw new NotFoundException();
+        }
     }
-    //POST /ninjas
+   //POST /ninjas
+    @Post()
+    @UseGuards(BeltGuard)
+    createNinja(@Body(new ValidationPipe()) CreateNinjaDto: CreateNinjaDto) {
+        return this.ninjasService.createNinja(CreateNinjaDto);
+    }
     //PUT /ninjas/:id--{...}
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
+        return this.ninjasService.UpdateNinja(+id, updateNinjaDto);
+    }
     //DELETE /ninjas/:id
+    @Delete(':id')
+    removeNinja(@Param('id') id: string){
+        return this.ninjasService.removeNinja(+id);
+    }
 }
